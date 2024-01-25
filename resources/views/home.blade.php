9dcfb42f6b9cc831994e2d3cb518b38a603822cc
AlexChihaia <alexchihaia84@gmail.com>
2024-01-25 19:19:05 +0100

    feat: hello laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+$message = 'Hello Laravel';
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Laravel</title>
+
+
+</head>
+
+<body>
+    <div>
+        <h1><?php echo $message ?></h1>
+    </div>
+
+</body>
+
+</html>
